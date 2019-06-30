cbb7d37797d8693060beb13fb06a733c384da579
Jarosław Terejko <j.terejko@gmail.com>
2019-07-01 01:41:29 +0200

    added animations changed new Date methods

--- app.js
+++ app.js
@@ -4,59 +4,78 @@ const name = document.querySelector("#name");
 const focus = document.querySelector("#focus");
 const hero = document.querySelector(".hero");
 const loader = document.querySelector(".loader");
-console.log(loader);
 
 setTimeout(() => {
   loader.remove();
-}, 1500);
+}, 3100);
 
 // time
 const displayTime = () => {
+  // old version with AM PM
+
+  // const now = new Date();
+
+  // let hour = now.getHours();
+  // const min = now.getMinutes();
+  // const sec = now.getSeconds();
+
+  // const amOrPm = hour >= 12 ? "PM" : "AM";
+
+  // hour = hour % 12 || 12;
+
+  // time.innerHTML = `${hour}<span>:</span>${fixTime(min)}<span>:</span>${fixTime(
+  //   sec
+  // )} ${amOrPm}`;
+
+  // new version
   const now = new Date();
 
-  let hour = now.getHours();
-  const min = now.getMinutes();
-  const sec = now.getSeconds();
-
-  const amOrPm = hour >= 12 ? "PM" : "AM";
-
-  hour = hour % 12 || 12;
-
-  time.innerHTML = `${hour}<span>:</span>${fixTime(min)}<span>:</span>${fixTime(
-    sec
-  )} ${amOrPm}`;
+  const currentTime = now.toLocaleTimeString();
+  console.log(currentTime);
+  time.innerHTML = `<span>${currentTime}</span>`;
 };
+
+// old version with AM PM
+// const fixTime = n => {
+//   return (parseInt(n, 10) < 10 ? "0" : "") + n;
+// };
 
 setBgAndGreeting = () => {
+  // const now = new Date("2019-06-13 19:22:13"); // testing
+
   const now = new Date();
-  //   const now = new Date("2019-06-13 20:22:13"); testing
-  let hour = now.getHours();
-  if (hour < 12) {
+  const hour = now.getHours();
+  if (hour < 4) {
+    hero.style.backgroundImage = "url(./img/night.jpg)";
+    greeting.textContent = "Witaj bardzo późną porą...";
+  } else if (hour < 8) {
     hero.style.backgroundImage = "url(./img/morning.jpg)";
-    greeting.textContent = "good morning";
+    greeting.textContent = "Witaj o wczesnym poranku.";
+  } else if (hour < 12) {
+    hero.style.backgroundImage = "url(./img/latemorning.jpg)";
+    greeting.textContent = "Dzień dobry.";
+  } else if (hour < 13) {
+    hero.style.backgroundImage = "url(./img/noon.jpg)";
+    greeting.textContent = "Witaj w południe.";
   } else if (hour < 18) {
     hero.style.backgroundImage = "url(./img/afternoon.jpg)";
-    greeting.textContent = "good afternoon";
+    greeting.textContent = "Witaj po południu";
   } else {
     hero.style.backgroundImage = "url(./img/evening.jpg)";
-    greeting.textContent = "good evening";
+    greeting.textContent = "Dobry wieczor";
   }
-};
-
-const fixTime = n => {
-  return (parseInt(n, 10) < 10 ? "0" : "") + n;
 };
 
 // get Name
 getUserName = () => {
   if (!localStorage.getItem("userName")) {
-    name.textContent = "Enter Your Name";
+    name.textContent = "Wpisz swoje imię";
   } else {
     name.textContent = localStorage.getItem("userName");
   }
 };
 
-setName = e => {
+setUserName = e => {
   if (e.type === "keypress") {
     console.log(e);
     if (e.code === "Enter") {
@@ -72,7 +91,7 @@ setName = e => {
 
 getUserFocus = () => {
   if (!localStorage.getItem("userFocus")) {
-    focus.textContent = "Enter Your Focus";
+    focus.textContent = "Wpisz swój cel";
   } else {
     focus.textContent = localStorage.getItem("userFocus");
   }
@@ -92,8 +111,8 @@ setFocus = e => {
   }
 };
 
-name.addEventListener("keypress", setName);
-name.addEventListener("blur", setName);
+name.addEventListener("keypress", setUserName);
+name.addEventListener("blur", setUserName);
 
 focus.addEventListener("keypress", setFocus);
 focus.addEventListener("blur", setFocus);
